b93ba46562a1931445263807cf96098701b3fce9
mike.melchior@outlook.com <mike.melchior@outlook.com>
2024-06-20 01:41:55 -0400

    test

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  
-]
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Mini Message Board", messages: messages });
 });
+
+router.get("/new", function (req, res, next) {
+	res.render("form", {title: "Message Form"})
+});
+
+router.post("/new", function(req, res, next) {
+	author = req.body.name;
+	messageText = req.body.message;
+	messages.push({text: messageText, user: author, added: new Date()})
+	res.redirect('/')
+})
 
 module.exports = router;
